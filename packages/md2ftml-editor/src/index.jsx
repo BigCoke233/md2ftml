0b770164c3a8a01abbeb462a3504075ccb0c3298
Eltrac <hi@guhub.cn>
2024-03-30 17:49:53 +0800

    Revert "feat(editor): try to implement prosemirror"
    This reverts commit 336173b44c9338bf9122e6367bd7a9e6eaf75d37.

--- packages/md2ftml-editor/src/index.jsx
+++ packages/md2ftml-editor/src/index.jsx
@@ -1,10 +1,21 @@
-import { ProseMirrorEditor } from './prosemirror';
+import './index.css';
+import React from 'react';
+import rehypeSanitize from 'rehype-sanitize';
+import MDEditor from '@uiw/react-md-editor';
+import { md2ftml } from '@md2ftml/parser';
 
 export default function () {
+  const [value, setValue] = React.useState('**Hello world!!!**');
   return (
     <div id={`md2ftml-editor`}>
-      <ProseMirrorEditor />
-      <textarea style={{ padding: '1rem' }} />
+      <MDEditor
+        value={value}
+        onChange={setValue}
+        previewOptions={{
+          rehypePlugins: [[rehypeSanitize]],
+        }}
+      />
+      <textarea style={{ padding: '1rem' }} value={md2ftml.parse(value)} />
     </div>
   );
 }
